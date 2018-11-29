b36324764c280b4502dc38f852af03db4060cbe1
izzy10b <34537734+izzy10b@users.noreply.github.com>
2018-11-29 15:43:32 +0000

    Updated Linked Lists

--- CA3VersionControl/src/LinkedList.java
+++ CA3VersionControl/src/LinkedList.java
@@ -1,4 +1,27 @@
 
 public class LinkedList {
+	private Node head;
+  	private int numItems;  // number of items in list
 
+
+  	public LinkedList()
+  	// creates an empty list
+  	{
+		head = null;
+		numItems = 0;
+
+  	}  // end default constructor
+
+  	public boolean isEmpty()
+  	// Determines whether a list is empty
+  	{
+		return (numItems == 0);
+
+  	} // end isEmpty
+
+  	public int size()
+  	// Returns the number of items that are in a list
+  	{
+  		return numItems;
+  	}  // end size
 }
