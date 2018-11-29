3f9d1a43eb177d15a0242e0e6a0919db39e9ea52
Samuel <Samuel@DESKTOP-MNA5C9K>
2018-11-29 16:45:07 +0000

    New Linklist with search

--- CA3VersionControl/src/LinkedList.java
+++ CA3VersionControl/src/LinkedList.java
@@ -1,7 +1,23 @@
 
-public class LinkedList {
-
-	int search;
-		
 	
-}
+      private Node Search(int index) {
+  	int i;
+  	Node currentNode;
+
+  	currentNode = head;
+  	for(i = 0; i < count; i++) {
+
+  	    if(i == index-1)
+  		return(currentNode);
+
+  	    // Oops, bad index
+  	    if(currentNode == null)
+  		return(null);
+
+  	    currentNode = currentNode.getNext();
+  	}
+
+  	
+  	
+  	
+
